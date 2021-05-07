18a09b9bdcb95bcee70491db1b7c20a4042e2e3e
GarryNeKasparov <vip.vlad.f.2002@gmail.com>
2021-05-07 21:57:34 +0300

    commit

--- paintApp/paintApp/asgi.py
+++ paintApp/paintApp/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for paint project.
+ASGI config for paintApp project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paintApp.settings')
 
 application = get_asgi_application()
--- paintApp/paintApp/wsgi.py
+++ paintApp/paintApp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for paint project.
+WSGI config for paintApp project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paint.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paintApp.settings')
 
 application = get_wsgi_application()
