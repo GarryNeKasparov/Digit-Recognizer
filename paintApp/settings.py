47de07ff577d80c06b8a6f06bb9b84e1afab048d
GarryNeKasparov <vip.vlad.f.2002@gmail.com>
2021-06-18 20:48:34 +0300

    S

--- paintApp/settings.py
+++ paintApp/settings.py
@@ -130,5 +130,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'paintApp/static'),
 )
---
+++ staticfiles/js/script.1f5e525ec62a.js
@@ -0,0 +1,36 @@
+window.addEventListener("load", function onWindowLoad() {
+    var canvas = document.getElementById("input");
+    var context = canvas.getContext("2d");
+    
+    context.fillStyle = "white";
+    context.lineCap = "round";
+    context.lineWidth = 16;
+    context.fillRect(0, 0, canvas.width, canvas.height)
+
+    document.getElementById("clear").onclick = function c() {
+        context.clearRect(0, 0, canvas.width, canvas.height);
+        //var status = document.getElementById("status");
+        //status.innerHTML = "Draw a digit";
+    };
+    document.getElementById("predict").onclick = function pred() {
+        $('form input[name=data]').val(canvas.toDataURL("image.png"));
+        $('form').submit();
+        //var status = document.getElementById("status");
+        //status.innerHTML = "I think it's";
+        
+    };
+    canvas.onmousemove = function drawIfPressed (e) {
+        var x = e.offsetX;
+        var y = e.offsetY;
+        var dx = e.movementX;
+        var dy = e.movementY;
+
+        if (e.buttons > 0) {
+            context.beginPath();
+            context.moveTo(x, y);
+            context.lineTo(x - dx, y - dy);
+            context.stroke();
+            context.closePath();
+        }
+    };
+});
